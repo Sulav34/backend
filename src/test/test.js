ec51efb9f8a78017388c734ea178d1e18df8fdd0
[Sulav Shrestha] <[Sulavstha34@gmail.com]>
2022-02-07 19:54:12 +0545

    backend

---
+++ src/test/test.js
@@ -0,0 +1,33 @@
+const Product = require("../models/productModel");
+const mongoose = require("mongoose");
+const url =
+  "mongodb+srv://sam:Imw8atfsIK8Lz8BA@ecommerce.wdwhq.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+beforeAll(async () => {
+  await mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  });
+});
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+describe("Product Schema test anything", () => {
+  // the code below is for insert testing
+  it("Add product testing anything", () => {
+    const product = {
+      name: "Nokia",
+      price: "2100",
+      quantity: "2",
+      description: "Good",
+      category:"Mobile",
+      subCategory:"Nokia Model",
+      brand:"Nokia"
+
+    };
+
+    // return Product.create(product).then((pro_ret) => {
+    //   expect(pro_ret.pname).toEqual("Nokia");
+    // });
+  });
+});
